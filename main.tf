0699e1bd3663d7c6a50e45bc95ca49ae71ce8509
= <arairikar1@gmail.com>
2025-06-05 17:22:05 -0500

    Adding S3 bucket website permission

--- main.tf
+++ main.tf
@@ -99,7 +99,8 @@ data "aws_iam_policy_document" "infra_role_inline_policy_document" {
       "s3:GetBucketPolicy",
       "s3:PutObject",
       "s3:GetBucketAcl",
-      "s3:GetBucketCORS"
+      "s3:GetBucketCORS",
+      "s3:GetBucketWebsite"
     ]
     resources = ["*"]
   }
