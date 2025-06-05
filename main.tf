6895c16e9f31eaddf92cf794cfa507eb5bf43e52
= <arairikar1@gmail.com>
2025-06-05 17:28:44 -0500

    Adding S3 bucket lifecycle permission

--- main.tf
+++ main.tf
@@ -103,7 +103,8 @@ data "aws_iam_policy_document" "infra_role_inline_policy_document" {
       "s3:GetBucketWebsite",
       "s3:GetAccelerateConfiguration",
       "s3:GetBucketRequestPayment",
-      "s3:GetBucketLogging"
+      "s3:GetBucketLogging",
+      "s3:GetLifecycleConfiguration"
     ]
     resources = ["*"]
   }
