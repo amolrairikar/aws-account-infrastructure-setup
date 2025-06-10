f235523211d44d9be61a0a80dcfa2b8017cedf3b
= <arairikar1@gmail.com>
2025-06-10 12:49:58 -0500

    Adding GitHub action code to zip lambda layer source code

--- main.tf
+++ main.tf
@@ -319,7 +319,7 @@ resource "aws_cloudtrail" "management_event_trail" {
 
 module "retry_api_call_layer" {
   source              = "git::https://github.com/amolrairikar/aws-account-infrastructure.git//modules/lambda-layer?ref=main"
-  layer_filename      = "retry_exceptions.zip"
+  layer_filename      = "retry_api_exceptions.zip"
   layer_name          = "retry_api_exceptions"
   layer_description   = "Layer for retrying API calls in Lambda functions"
   layer_architectures = ["x86_64"]
