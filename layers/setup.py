d323aac24416bc0d7e066d16f95d055fedba827e
= <arairikar1@gmail.com>
2025-06-11 21:49:40 -0500

    Updating directory paths

--- layers/setup.py
+++ layers/setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='lambda_layers',
     version='0.1.0',
-    package_dir={'': 'layers'},
-    packages=find_packages(where='layers'),
+    package_dir={'': '.'},
+    packages=find_packages(where='.'),
     install_requires=[
         'backoff',
         'boto3',
