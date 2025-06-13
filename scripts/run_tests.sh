2b24c3b7207b15c68543c7a25475e8d5dde69e9c
= <arairikar1@gmail.com>
2025-06-12 21:58:30 -0500

    Adding reusable test script

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
-# Run unit tests
+set -e
+
 echo "Running unit tests..."
-if ! pipenv run coverage run --source=layers -m unittest discover -s tests; then
-    echo "Unit tests failed."
+if ! pipenv run coverage run --source=lambdas -m unittest discover -s tests/unit -v; then
+    echo "Unit tests failed!"
     exit 1
 fi
-echo "Unit tests passed."
+mv .coverage .coverage.unit
 
-# Generate coverage report
+COMPONENT_TEST_DIR="tests/component"
+
+if [ -d "$COMPONENT_TEST_DIR" ] && [ "$(find "$COMPONENT_TEST_DIR" -type f -name "*.py" | wc -l)" -gt 0 ]; then
+    echo "Running component tests..."
+    if ! pipenv run coverage run --source=lambdas -m unittest discover -s "$COMPONENT_TEST_DIR" -v; then
+        echo "Component tests failed!"
+        exit 1
+    fi
+    mv .coverage .coverage.component
+    echo "Combining coverage data..."
+    pipenv run coverage combine .coverage.unit .coverage.component
+else
+    echo "No component tests found. Skipping..."
+    cp .coverage.unit .coverage
+fi
+
 echo "Generating coverage report..."
-pipenv run coverage report --omit="tests/*" --fail-under=80
+pipenv run coverage report --omit=lambdas/write_train_lines/retry_api_exceptions.py --fail-under=80
 
 echo "Test execution complete!"
