d676b35466ec11827ba0eeb9e021ab871fc45cec
= <arairikar1@gmail.com>
2025-06-25 14:46:16 -0500

    Adding glue tagging permissions

--- main.tf
+++ main.tf
@@ -265,7 +265,9 @@ data "aws_iam_policy_document" "infra_role_inline_policy_document" {
       "glue:*Table",
       "glue:*TableVersion",
       "glue:*TableVersions",
-      "glue:*Database"
+      "glue:*Database",
+      "glue:TagResource",
+      "glue:UntagResource"
     ]
     resources = [
       "arn:aws:glue:${var.aws_region_name}:${data.aws_caller_identity.current.account_id}:catalog",
