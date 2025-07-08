83596e5d9e2ceb53181d77c064ec6d4d9d7e98ab
= <arairikar1@gmail.com>
2025-07-07 21:55:15 -0500

    Adding new scheduler ARN to infra role permissions

--- main.tf
+++ main.tf
@@ -65,7 +65,8 @@ data "aws_iam_policy_document" "infra_role_inline_policy_document" {
     ]
     resources = [
       "arn:aws:scheduler:us-east-2:${data.aws_caller_identity.current.account_id}:schedule/default/spotify-listening-history-lambda-trigger",
-      "arn:aws:scheduler:us-east-2:${data.aws_caller_identity.current.account_id}:schedule/default/cta-write-train-lines-lambda-trigger"
+      "arn:aws:scheduler:us-east-2:${data.aws_caller_identity.current.account_id}:schedule/default/cta-write-train-lines-lambda-trigger",
+      "arn:aws:scheduler:us-east-2:${data.aws_caller_identity.current.account_id}:schedule/default/cta-bucket-raw-data-lambda-trigger"
     ]
   }
   statement {
